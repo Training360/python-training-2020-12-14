6b88a2de0d1d096658e1afd788052d350e144821
iviczian <viczian.istvan@gmail.com>
2020-12-14 12:56:41 +0100

    Print függvény és változók használata

---
+++ hello.py
@@ -0,0 +1,15 @@
+# Nyelvi szabályok : szintaktikája
+# Automatikusan ellenőrzni lehet
+# author: Viczian Istvan
+# Clean code
+# v1
+# v2 - modositottam a kiirast - Viczian Istvan
+
+# Demo celbol
+print('Hello World!') # Elso programja mindenkinek
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+print('Hello World!')
+# Szemantika
+# Unit tesztek
---
+++ variables.py
@@ -0,0 +1,70 @@
+# nyelvtan
+# szintaktika
+# darabkák
+# egy darabka: literál = konstans érték
+
+n = 5
+print(n)
+
+n = 6
+print(n) # függvényhívás
+
+s = 'Hello World'
+print(s)
+
+m = n
+print(m)
+
+n = 'Hello'
+print(n)
+
+employee_name = 'John Doe'
+
+print(type(7))
+print(type(8))
+print(type(8.8))
+print(type('Jane Doe'))
+
+# Python típusos nyelv
+
+n = 'Hello'
+print(type(n))
+
+n = 5
+print(type(n))
+
+# Dinamikus típus
+
+# print(x) INVALID
+
+s = "Hello"
+# s = 'Istvan's' NEM JO
+s = 'Istvan\'s'
+print(s)
+
+s = "Istvan's"
+
+s = """
+Alma
+Korte
+Barack
+"""
+
+print(s)
+
+s = """
+<a>
+<b>
+</b>
+</a>
+"""
+
+print(s)
+
+s = """
+{
+    'name': 'John Doe'
+}
+"""
+
+print(s)
